4566ab78db07a168c5acc5b15ce4db0ce12a7b0a
Phuvo <phuvo1288@gmail.com>
2025-01-13 23:55:28 +0700

    Thêm Router cho web và thêm layout cho admin

---
+++ MiroP_Ecomic/Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiroP_Ecomic.Core.Constants;
+
+namespace MiroP_Ecomic_Web.Areas.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route(PortalEndpoint.Home.HomeEndpoint)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MiroP_Ecomic/Web/Modules/AutofacModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace MiroP_Ecomic_Web.Modules
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
