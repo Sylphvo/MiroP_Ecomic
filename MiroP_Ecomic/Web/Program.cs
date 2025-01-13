30c5096ae43e239e67976828185a8d9f9ebb4d01
Phuvo <phuvo1288@gmail.com>
2025-01-13 23:42:29 +0700

    Build xong đường dẫn thư mục admin và client

--- MiroP_Ecomic/Web/Program.cs
+++ MiroP_Ecomic/Web/Program.cs
@@ -1,7 +1,27 @@
+
+using Autofac.Core;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MiroP_Ecomic_Web.Modules;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Configure services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+
+// Configure Autofac-specific registrations
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    // Register your services with Autofac
+    containerBuilder.RegisterType<AutofacModule>().As<AutofacModule>();
+});
+
 
 var app = builder.Build();
 
@@ -23,5 +43,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.Run();
